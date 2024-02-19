e01df30fc7d4245d22028086632f3001a4838d68
Mariana <macbedoya@gmail.com>
2024-02-19 15:14:21 -0500

    :sparkles: typing an http response

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,10 +1,12 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+  public gifList: Gif[] = []
   private _tagsHistory: string[] = [];
   private apiKey: string = "rrd78KIZbOMF9l54ckuUQ7Pz5U3P4Uki"
   private serviceUrl: string = "https://api.giphy.com/v1/gifs"
@@ -23,7 +25,7 @@ export class GifsService {
     this._tagsHistory = this._tagsHistory.splice(0, 10);
   }
 
-  async searchTag(tag: string): Promise<void> {
+  searchTag(tag: string):void {
     if (tag.length === 0) return;
     this.organiceHistory(tag);
     const params = new HttpParams()
@@ -31,12 +33,10 @@ export class GifsService {
       .set('limit', '10')
       .set('q', tag)
 
-    this.http.get(`${this.serviceUrl}/search`, { params })
-      .subscribe((resp: any) => {
-        console.log(resp.data);
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe(resp => {
+        this.gifList = resp.data;
+        console.log(this.gifList)
       })
-    // fetch('https://api.giphy.com/v1/gifs/search?api_key=rrd78KIZbOMF9l54ckuUQ7Pz5U3P4Uki&q=valorant&limit=10')
-    // .then(resp => resp.json())
-    // .then(data => console.log(data))
   }
 }
