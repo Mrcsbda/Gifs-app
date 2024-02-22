8f3245abf39fddebcaa0b333efcab600020add0e
Mariana <macbedoya@gmail.com>
2024-02-22 11:58:07 -0500

    lady image was done

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html'
+})
+
+export class LazyImageComponent implements OnInit {
+  @Input() public url!: string;
+  @Input() public alt: string = '';
+
+  hasLoaded: boolean = false;
+  ngOnInit(): void {
+    if (!this.url) throw new Error('Url property is required');
+  }
+
+  onLoad(): void {
+    console.log('loaded');
+    this.hasLoaded = true;
+  }
+
+}
